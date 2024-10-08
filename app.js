83e6f79fb8ea8d9828392682513b3e15f3cddfad
vikas rahangdale <vikasrahnagdale@5606.gmail.com>
2024-10-08 11:46:55 +0530

    first commit

--- app.js
+++
@@ -1,7 +0,0 @@
-var cities = [23,45,65,78,90,78]
-
-var sum = 0;
-for(var i = 0; i < 6; sum+1){
-    sum +=[i];
-    console.log(sum)
-}
---
+++ solve.js
@@ -0,0 +1,5 @@
+var com = ['micro','apple','netlink','infoses']
+   
+com.slice(1,'aaa')
+
+console.log(com)
